7f50ca971c5c054cfe5101c388d6757eaffb72c4
vipulbitbucket <vipulshrivastav@anylinuxwork.com>
2016-12-29 15:01:20 +0530

    Added wordfrence plugin of WW site  Version: 6.0.24

---
+++ wp-content/plugins/wordfence/tmp/configCache.php
@@ -0,0 +1,4 @@
+<?php
+/* Wordfence temporary file security header */
+echo "Nothing to see here!\n"; exit(0);
+?>a:9:{s:9:"cacheType";s:0:"";s:9:"howGetIPs";s:0:"";s:11:"whitelisted";s:0:"";s:6:"apiKey";s:160:"10c58217da960ee4917f4e7d01edb976d046541a755c05f6183b821d2c2896916e19926e9b385ff96242a05af433a4320f47f7d0d316b37275c208bca872125c3d57b83e06fe37e2e262cf0a2dca2fb2";s:20:"loginSec_lockoutMins";s:1:"5";s:19:"other_hideWPVersion";s:1:"0";s:15:"firewallEnabled";s:1:"1";s:18:"liveTrafficEnabled";s:1:"1";s:14:"disableCookies";s:1:"0";}
